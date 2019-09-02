1a4d6d066ec420204ddcdbc1675e8cd6e33e4048
Charles Kato <kcharles52@gmail.com>
2019-09-02 13:58:37 +0300

    feature(schema): type relationship
    - add relationship between types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,14 +6,18 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
 const books = [
-  { name: 'Name of the wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Lnog Earth', genre: 'Sci-Fi', id: '3' }
+  { name: 'Name of the wind', genre: 'Fantasy', id: '1', authorID: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorID: '2' },
+  { name: 'The Lnog Earth', genre: 'Sci-Fi', id: '3', authorID: '3' },
+  { name: 'Name of the wind', genre: 'Fantasy', id: '4', authorID: '1' },
+  { name: 'The Final Match', genre: 'Fantasy', id: '5', authorID: '3' },
+  { name: 'The Long Journey', genre: 'Sci-Fi', id: '6', authorID: '3' }
 ];
 const authors = [
   { name: 'Patrick Viera', age: 44, id: '1' },
@@ -26,7 +30,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorID });
+      }
+    }
   })
 });
 
@@ -35,9 +45,16 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorID: parent.id });
+      }
+    }
   })
 });
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
