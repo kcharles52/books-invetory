5e9f477c1edefd3c4ed97c329ede8f65f054f047
Kato Charles <kcharles52@gmail.com>
2019-09-02 12:47:33 +0300

    feature(schema): schemas and types (#1)
    - add book type
    - add author type
    - add root query
    - create schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
-const port = process.env.port || 4000;
+const port = process.env.port || 4001;
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
-})
+});
