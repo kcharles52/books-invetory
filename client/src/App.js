72ebf459b931465041e4c096e0d2fc64a0ca41b8
Kato Charles <kcharles52@gmail.com>
2019-09-04 14:49:55 +0300

    chore(client): initial client setup (#6)
    - add react app to the project
    - add book list component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import BookList from "./components/BookList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>KT20 Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
